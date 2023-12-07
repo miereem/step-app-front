bd97950a0072fbcd9814a11e7b001ac8cdfdaa33
miereem <mabdurasulkyzy@gmail.com>
2023-12-07 13:15:06 +0300

    index page in progress, but ij is blocking me

--- src/Pages/IndexPage.jsx
+++ src/Pages/IndexPage.jsx
@@ -1,4 +1,28 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Image from "../Image.jsx";
+
 export default function IndexPage() {
-    return ('index page');
+    const [places, setPlaces] = useState([]);
+    useEffect(() => {
+        axios.get('/places').then(response => {
+            setPlaces([...response.data, ...response.data, ...response.data, ...response.data]);
+        });
+    }, []);
+    return (
+        <div className="mt-8 gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {places.length > 0 && places.map( place => (
+                    <div className="rounded-xl p-2">
+                        <div className="">
+                            {place.photos?.[0] && (
+                                <Image className="rounded-xl" src={place.photos[0]} alt={''}/>
+                            )}
+                        </div>
+                        <h2 className="text-xs font-bold underline py-1 leading-4">{place.address}</h2>
+                        <h2 className="leading-4">{place.title}</h2>
+                    </div>
+                ))}
+        </div>
+    );
 
 }
